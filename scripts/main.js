98c76b2bf15b0dea1b0a01c44d32ca4305af6365
Cody Clark <codyjamesclark@gmail.com>
2024-06-11 14:15:42 -0500

    added base actor module to display names and added some basic css

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
+import { actorHTML } from "./actors.js";
 
+const content = document.querySelector("#content")
+
+const applicationHTML = `
+    <h1>Magnum TV</h1>
+    <article class="details">
+        <section class="detail--column">
+            <h2>Actors</h2>
+            ${actorHTML()}
+        </section>
+        <section class="detail--column">
+            <h2>Genres</h2>
+            
+        </section>
+    </article>
+
+    <article id="tvshows">
+        <h2>Tv Shows</h2>
+        
+    </article>
+`
+
+content.innerHTML = applicationHTML
